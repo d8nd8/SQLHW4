7ef6577a16e61c8cec4e760e5c255cd3b3d87a59
d8nd8 <jumppmuj@yandex.ru>
2024-08-14 20:54:00 +0300

    Rework added

--- find_shop.py
+++ find_shop.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 from sqlalchemy import create_engine
 from HW4 import create_tables, Publisher, Sale, Shop, Stock, Book
 
-def get_sales_by_publisher(publisher_name, session):
+def get_shops(publisher_identifier, session):
 
     sales_info = (
         session.query(
@@ -16,12 +16,16 @@ def get_sales_by_publisher(publisher_name, session):
         .join(Sale, Stock.id == Sale.id_stock)
         .join(Shop, Stock.id_shop == Shop.id)
         .join(Publisher, Book.id_publisher == Publisher.id)
-        .filter(Publisher.name == publisher_name)
-        .all()
     )
 
-    for sale in sales_info:
-        print(f"{sale.title} | {sale.name} | {sale.price} | {sale.date_sale}")
+    if publisher_identifier.isdigit():
+        results = sales_info.filter(Publisher.id == int(publisher_identifier)).all()
+    else:
+        results = sales_info.filter(Publisher.name == publisher_identifier).all()
+
+
+    for book_title, shop_name, sale_price, sale_date in results:
+        print(f"{book_title: <40} | {shop_name: <10} | {sale_price: <8} | {sale_date.strftime('%d-%m-%Y')}")
 
 if __name__ == "__main__":
 
@@ -30,7 +34,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    publisher_name = input("Введите имя издателя: ")
-    get_sales_by_publisher(publisher_name, session)
+
+    publisher_name = input("Введите имя издателя или его ID: ")
+    get_shops(publisher_name, session)  # Вызываем функцию для получения данных
 
     session.close()
